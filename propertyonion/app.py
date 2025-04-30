ec59998c9786d1a943e2c60f1f5c93313193cfe0
pritesh ramani <ramani2020p@gmail.com>
2025-04-30 11:38:16 +0530

    update app for scrap other rows and interfaces are also changed

--- propertyonion/app.py
+++ propertyonion/app.py
@@ -8,12 +8,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 # Function to fetch page source from URL
-def download_page_source(url="https://propertyonion.com/property_search", delay=5):
+def download_page_source(url="https://propertyonion.com/property_search"):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # Optional: run in headless mode
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get(url)
-    time.sleep(delay)
+    time.sleep(5)  # Wait for the page to load
     page_source = driver.page_source
     driver.quit()
     return page_source
@@ -22,6 +22,7 @@ def download_page_source(url="https://propertyonion.com/property_search", delay=
 def scrape_real_estate_data(file_content):
     soup = BeautifulSoup(file_content, "html.parser")
     fullpanel = soup.find_all("div", class_="addressPanel flex justify-content-between align-items-center")
+    list=soup.find_all("div", class_="infoBar flex justify-content-between px-1 pt-2 pb-2")
 
     data = [panel.text.strip() for panel in fullpanel]
     rows = []
@@ -37,7 +38,23 @@ def scrape_real_estate_data(file_content):
         sqft_value = sqft.group(1) if sqft else 'N/A'
         address_value = address
 
-        rows.append({'Beds': beds_value, 'Baths': baths_value, 'Sqft': sqft_value, 'Address': address_value})
+        for i in range(len(list)):
+            text=list[i].text.strip()
+            match = re.match(r'^(\w+)\s+(.*?)(\d{2}/\d{2}/\d{4})$', text)
+            if match:
+                status = match.group(1).strip()
+                listing = match.group(2).strip()
+                date = match.group(3)
+            # print("Status:", status)
+            # print("Label:", listing)
+            # print("Date:", date)
+            else:
+                status = 'N/A'
+                listing = 'N/A'
+                date = 'N/A'
+    
+        rows.append({'Beds': beds_value, 'Baths': baths_value, 'Sqft': sqft_value, 'Address': address_value,'Status': status, 'Listing Type': listing, 'Date': date})
+
 
     return pd.DataFrame(rows)
 
@@ -45,25 +62,52 @@ def scrape_real_estate_data(file_content):
 st.title("🏠 Property Onion Scraper")
 
 # Section to download HTML
-if st.button("Download Page Source"):
-    html_content = download_page_source()
-    st.success("Page source downloaded successfully!")
+# if st.button("Download Page Source"):
+#     html_content = download_page_source()
+#     st.success("Page source downloaded successfully!")
 
+#     st.download_button(
+#         label="📄 Download HTML File",
+#         data=html_content,
+#         file_name="page_source.txt",
+#         mime="text/plain"
+#     )
+
+# st.markdown("---")
+
+
+# Upload and scrape
+# uploaded_file = st.file_uploader("Upload a page_source.txt file", type="txt")
+
+# if uploaded_file:
+#     file_content = uploaded_file.read().decode("utf-8")
+
+#     if st.button("Scrape Data"):
+#         df = scrape_real_estate_data(file_content)
+#         st.success("Scraping completed!")
+#         st.dataframe(df)
+
+#         csv = df.to_csv(index=False).encode("utf-8")
+#         st.download_button(
+#             label="📥 Download CSV",
+#             data=csv,
+#             file_name="meta.csv",
+#             mime="text/csv"
+#         )
+
+
+#  optinons for enter url
+url = st.text_input("Enter a link", placeholder="https://propertyonion.com/property_search")
+if url:
+    st.button("Download Page Source")
+    file_content= download_page_source(url)
+    st.success("Page source downloaded successfully!")
     st.download_button(
         label="📄 Download HTML File",
-        data=html_content,
+        data=file_content,
         file_name="page_source.txt",
         mime="text/plain"
     )
-
-st.markdown("---")
-
-# Upload and scrape
-uploaded_file = st.file_uploader("Upload a page_source.txt file", type="txt")
-
-if uploaded_file:
-    file_content = uploaded_file.read().decode("utf-8")
-
     if st.button("Scrape Data"):
         df = scrape_real_estate_data(file_content)
         st.success("Scraping completed!")
@@ -76,3 +120,4 @@ if uploaded_file:
             file_name="meta.csv",
             mime="text/csv"
         )
+    

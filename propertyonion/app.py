139e8cbb55fcf96786d39c684b4dceae8739ebd8
Pritesh Patel <128299142+priteshramani@users.noreply.github.com>
2025-04-30 12:04:57 +0530

    Update app.py

--- propertyonion/app.py
+++ propertyonion/app.py
@@ -12,7 +12,7 @@ def download_page_source(url="https://propertyonion.com/property_search"):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # Optional: run in headless mode
     # chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
 
     chrome_options.binary_location = "/usr/bin/chromium-browser"
     

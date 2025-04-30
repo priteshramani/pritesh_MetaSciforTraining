7182d0639386fff036479ff1ed766eff4efe3a1e
Pritesh Patel <128299142+priteshramani@users.noreply.github.com>
2025-04-30 15:19:58 +0530

    Update app.py
    this is an for status listing type and date correction

--- propertyonion/app.py
+++ propertyonion/app.py
@@ -11,8 +11,10 @@ import time
 def download_page_source(url="https://propertyonion.com/property_search"):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # Optional: run in headless mode
+    options.add_argument("--no-sandbox")
+    # options.binary_location = "/usr/bin/chromium-browser"
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    time.sleep(5)  # Wait for the driver to initialize
     driver.get(url)
     time.sleep(5)  # Wait for the page to load
     page_source = driver.page_source
@@ -22,42 +24,76 @@ def download_page_source(url="https://propertyonion.com/property_search"):
 # Function to scrape the HTML content
 def scrape_real_estate_data(file_content):
     soup = BeautifulSoup(file_content, "html.parser")
-    fullpanel = soup.find_all("div", class_="addressPanel flex justify-content-between align-items-center")
-    list=soup.find_all("div", class_="infoBar flex justify-content-between px-1 pt-2 pb-2")
+    addressbar = soup.find_all("div", class_="addressPanel flex justify-content-between align-items-center")
+    datebar=soup.find_all("div", class_="infoBar flex justify-content-between px-1 pt-2 pb-2")
 
-    data = [panel.text.strip() for panel in fullpanel]
     rows = []
 
-    for item in data:
-        beds = re.search(r'(\d+(?:\.\d+)?) Beds', item)
-        baths = re.search(r'(\d+(?:\.\d+)?) Baths', item)
-        sqft =  re.search(r'([\d,]+) sqft', item)
-        address = item.split(' Beds')[0]
+    for i in range(len(datebar)):
+        data=addressbar[i].text.strip()
+        beds = re.search(r'(\d+(?:\.\d+)?) Beds', data)
+        baths = re.search(r'(\d+(?:\.\d+)?) Baths', data)
+        sqft =  re.search(r'([\d,]+) sqft', data)
+        address = data.split(' Beds')[0]
 
         beds_value = int(beds.group(1)[-1]) if beds else 'N/A'
         baths_value = baths.group(1) if baths else 'N/A'
         sqft_value = sqft.group(1) if sqft else 'N/A'
         address_value = address
 
-        for i in range(len(list)):
-            text=list[i].text.strip()
-            match = re.match(r'^(\w+)\s+(.*?)(\d{2}/\d{2}/\d{4})$', text)
-            if match:
-                status = match.group(1).strip()
-                listing = match.group(2).strip()
-                date = match.group(3)
-            else:
-                status = 'N/A'
-                listing = 'N/A'
-                date = 'N/A'
-    
-        rows.append({'Beds': beds_value, 'Baths': baths_value, 'Sqft': sqft_value, 'Address': address_value,'Status': status, 'Listing Type': listing, 'Date': date})
+        
+        text=datebar[i].text.strip()
+        match = re.match(r'^(\w+)\s+(.*?)(\d{2}/\d{2}/\d{4})$', text)
+        if match:
+            status = match.group(1).strip()
+            listing = match.group(2).strip()
+            date = match.group(3)
+        else:
+            status = 'N/A'
+            listing = 'N/A'
+            date = 'N/A'
 
+        rows.append({'Beds': beds_value, 'Baths': baths_value, 'Sqft': sqft_value, 'Address': address_value,'Status': status, 'Listing Type': listing, 'Date': date})
 
     return pd.DataFrame(rows)
 
 # Streamlit UI
 st.title("🏠 Property Onion Scraper")
+
+# Section to download HTML
+# if st.button("Download Page Source"):
+#     html_content = download_page_source()
+#     st.success("Page source downloaded successfully!")
+
+#     st.download_button(
+#         label="📄 Download HTML File",
+#         data=html_content,
+#         file_name="page_source.txt",
+#         mime="text/plain"
+#     )
+
+# st.markdown("---")
+
+
+# Upload and scrape
+# uploaded_file = st.file_uploader("Upload a page_source.txt file", type="txt")
+
+# if uploaded_file:
+#     file_content = uploaded_file.read().decode("utf-8")
+
+#     if st.button("Scrape Data"):
+#         df = scrape_real_estate_data(file_content)
+#         st.success("Scraping completed!")
+#         st.dataframe(df)
+
+#         csv = df.to_csv(index=False).encode("utf-8")
+#         st.download_button(
+#             label="📥 Download CSV",
+#             data=csv,
+#             file_name="meta.csv",
+#             mime="text/csv"
+#         )
+
 
 #  optinons for enter url
 url = st.text_input("Enter a link", placeholder="https://propertyonion.com/property_search")
@@ -81,7 +117,7 @@ if url:
     st.download_button(
         label="📥 Download CSV",
         data=csv,
-        file_name="meta.csv",
+        file_name="output.csv",
         mime="text/csv"
     )
     
